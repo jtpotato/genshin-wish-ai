e3e19732e93247f1acbdbc2f0b5fcc8ab52c42a9
Joel <58995538+jtpotato@users.noreply.github.com>
2023-01-04 17:20:58 +1100

    abandon the ai idea

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-AI project to work out how successful a hypothetical person would be in gambling.
-
-Most certainly not financial advice, gamble responsibly, *please*.
-
-# Process
-- Create simulator (`generate.ts`)
-- Upload results (`results.json`) to GitHub.
-- Move to Google Colab to perform linear regression??? i have no clue what I'm saying.
-- Colab file will be uploaded to GitHub for you guys 😄
+# Genshin Wish Simulator.
+For fun. Good luck on your banners I guess??? This is definitely not going to be accurate - it's a probability simulator, not a fortune teller.
--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,6 +1,4 @@
 // Generates a CSV of wishing data, because I don't have an actual dataset. It's basically a simulator.
-import fs from 'fs';
-
 const baseRate = 0.006; // 0.6%
 const softPityIncrease = (1 - 0.006) / 15; // assuming linear increase
 
@@ -38,16 +36,3 @@ const simulate = (currentPity = 0) => {
     standard: standardCharacter
   })
 };
-
-for (let i = 0; i < 1000; i++) {
-    let starting = Math.floor(Math.random() * 90);
-    simulate(starting);
-}
-
-let saveObject = results
-
-let jsonSaveObject = JSON.stringify(saveObject);
-
-fs.writeFile('results.json', jsonSaveObject, () => {});
-
-console.log(results);
